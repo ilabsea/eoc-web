d5259ef82b1cc28b0ee3bf1dc04a501b5f6a7b7e
radinreth <radin.reth@gmail.com>
2019-09-20 16:38:44 +0700

    split api controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sops#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :sops, only: [:index]
+    end
+  end
+
   resources :sops do
     get :download, on: :collection
   end
