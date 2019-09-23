635ad50f7e7583e9b39a2e1ceb3901a5e012360e
radinreth <radin.reth@gmail.com>
2019-09-23 14:06:35 +0700

    Merge autoload option

--- app/controllers/api/v1/sops_controller.rb
+++ app/controllers/api/v1/sops_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
   class SopsController < ApiController
     def index
-      @search = Sop.search({keyword: params['searchText']}).results.results
+      @search = Sop.search({keyword: params['searchText'], from: params['from'], size: params['size']}).results.results
       respond_to do |format|
         format.json { render json: @search }
       end
