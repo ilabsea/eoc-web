196594b9972c53224f792e98d532eb6398044459
radinreth <radin.reth@gmail.com>
2019-10-04 11:10:07 +0700

    Update api call for spreadsheet service

--- app/models/sop.rb
+++ app/models/sop.rb
@@ -16,11 +16,10 @@ class Sop < ApplicationRecord
     SopSearchResultPresenter.new(response).results
   end
 
-  def with_attachment(file)
-    return unless document?
+  def with_attachment(path, file)
+    return if folder? || file.blank?
     return self.remote_file_url = file if URI.parse(file).absolute?
 
-    path = "public/uploads/roo_test/#{file}"
-    File.open(path) { |f| yield f } if File.exist?(path)
+    File.open("#{path}/#{file}") { |f| yield f } if File.exist?("#{path}/#{file}")
   end
 end
