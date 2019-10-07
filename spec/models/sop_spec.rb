9f60b16ffab430ec19813e8ef4805eff7dc50ecf
radinreth <radin.reth@gmail.com>
2019-10-07 10:04:52 +0700

    add sop spec

--- spec/models/sop_spec.rb
+++ spec/models/sop_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Sop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { expect(subject.class).to respond_to(:search_highlight) }
+  it { is_expected.to respond_to(:with_attachment) }
+
+  describe 'attributes' do
+    it { should define_enum_for(:document_type).with_values([:document, :folder]) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+  end
+
+  describe 'association' do
+    it { should belong_to(:category).optional }
+  end
 end
