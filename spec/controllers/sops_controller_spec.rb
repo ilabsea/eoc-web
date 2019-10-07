a2542452e6b7f86739fb822a1cba01ad461654b7
radinreth <radin.reth@gmail.com>
2019-10-07 22:18:08 +0700

    Update more SOP controller spec

--- spec/controllers/sops_controller_spec.rb
+++ spec/controllers/sops_controller_spec.rb
@@ -8,5 +8,33 @@ RSpec.describe SopsController, type: :controller do
 
       expect(response.status).to eq 200
     end
+
+    it 'returns results from elasticsearch'
+  end
+
+  describe 'GET new' do
+    it 'shows new' do
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'POST create' do
+    it 'create new sop' do
+      post :create, params: { sop: { name: 'test', category_id: nil, tags: 'tags', file: nil } }
+      expect(subject).to redirect_to(sops_path)
+    end
+
+    it 'cannot create SOP with duplicate name' do
+      sop = create(:sop, name: 'dup')
+      post :create, params: { sop: { name: 'dup', category_id: nil, tags: 'tags', file: nil } }
+
+      expect(subject).to render_template(:new)
+    end
+  end
+
+  describe 'GET edit' do
+    it 'shows edit' do
+      expect(response.status).to eq 200
+    end
   end
 end
