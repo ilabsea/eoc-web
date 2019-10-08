9bca4f14c2e3f3696a05407b57e88dddf77f207e
radinreth <radin.reth@gmail.com>
2019-10-08 08:53:40 +0700

    add spec for update action & add annotate model gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id             :bigint           not null, primary key
+#  name           :string
+#  parent_id      :integer
+#  lft            :integer          not null
+#  rgt            :integer          not null
+#  depth          :integer          default(0), not null
+#  children_count :integer          default(0), not null
+#
+
 class Category < ApplicationRecord
 end
--- spec/factories/sops.rb
+++ spec/factories/sops.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: sops
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  file          :string
+#  tags          :text
+#  category_id   :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  document_type :integer          default("document")
+#
+
 FactoryBot.define do
   factory :sop do
     name { 'name' }
