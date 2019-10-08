8e8bda71b5a46d80905792b42c6af31f13682919
radinreth <radin.reth@gmail.com>
2019-10-08 11:48:06 +0700

    Add spec handling whitelist files

--- spec/services/spreadsheet_service_spec.rb
+++ spec/services/spreadsheet_service_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe SpreadsheetService, type: :model do
   let(:not_found) { file_path('404.zip') }
-  let(:blacklist) { file_path('blacklist.mp3') }
+  let(:blacklist) { file_path('angkorwat.jpg') }
 
   it 'not found' do
     service = described_class.new(not_found)
-    expect { service.unzip }.to raise_error(RuntimeError, I18n.t('.spreadsheet_service.not_found', filename: File.basename(not_found)))
+    error_msg = I18n.t('.spreadsheet_service.not_found', filename: File.basename(not_found))
+
+    expect { service.unzip }.to raise_error(RuntimeError, error_msg)
+  end
+
+  it 'is not in whitelist' do
+    service = described_class.new(blacklist)
+    whitelist = described_class.class_variable_get(:@@WHITELIST)
+    error_msg = I18n.t('.spreadsheet_service.blacklist_ext', allowance: whitelist.to_sentence)
+
+    expect { service.unzip }.to raise_error(RuntimeError, error_msg)
   end
 end
