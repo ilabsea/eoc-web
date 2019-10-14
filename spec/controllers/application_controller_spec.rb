3626c81f75eebcb49d614d668751d84640ee3ecb
Borey Lim <lim.borey@gmail.com>
2019-10-14 17:02:57 +0700

    implement user authentication

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+# test only Active Record
+RSpec.describe ApplicationController, type: :controller do
+  controller do
+    def index; end
+  end
+
+  describe 'user not login' do
+    it 'redirect to signin page' do
+      get :index
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+end
+
