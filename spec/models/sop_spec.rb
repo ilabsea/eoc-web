5f733a71c281bd6af61b8bd77efe174977d1108b
radinreth <radin.reth@gmail.com>
2019-10-21 16:01:22 +0700

    fix sop spec

--- spec/models/sop_spec.rb
+++ spec/models/sop_spec.rb
@@ -18,10 +18,6 @@ RSpec.describe Sop, type: :model do
   it { expect(subject.class).to respond_to(:search_highlight) }
   it { is_expected.to respond_to(:with_attachment) }
 
-  describe 'attributes' do
-    it { should define_enum_for(:document_type).with_values([:document, :folder]) }
-  end
-
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
