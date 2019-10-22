39b87575aab45d2051446eda621d0e6c7669f940
radinreth <radin.reth@gmail.com>
2019-10-22 16:39:20 +0700

    fixed PushNotificationService spec

--- spec/services/push_notification_service_spec.rb
+++ spec/services/push_notification_service_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 require_relative '../support/fcm_fake'
 
 RSpec.describe PushNotificationService do
-  let(:options) { { title: 'Attention!', body: 'H1N1 has new symptom!' } }
-  let(:fake) { FCMFake.new('xxx') }
+  context 'send' do
+    let(:options) { { title: 'Attention!', body: 'H1N1 has new symptom!' } }
+    let(:fake) { FCMFake.new('xxx') }
+    let(:notification) { described_class.new(options) }
+    let(:token_ids) { %w(a1 b2 c3 d4 e5) }
 
-  before(:each) {
-    @notification = described_class.new(options)
-  }
+    before(:each) {
+      allow(notification).to receive(:fcm).and_return(fake)
+      stub_const("#{described_class}::SEND_THRESHOLD", 2)
+    }
 
-  it 'chunks senders #prepare' do
-    token_ids = %w(a1 b2 c3 d4 e5)
+    it 'chunks senders #prepare' do
+      expect(notification).to receive(:send).exactly(3).times
 
-    allow(@notification).to receive(:fcm) { FCMFake.new('xxx') }
-    expect( @notification ).to receive(:send).exactly(3).times
-
-    @notification.notify token_ids
+      notification.notify token_ids
+    end
   end
 end
