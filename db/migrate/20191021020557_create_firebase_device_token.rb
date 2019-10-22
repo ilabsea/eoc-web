c982f78cfec5d6d742d702059e056d24a864f9c9
radinreth <radin.reth@gmail.com>
2019-10-22 16:13:26 +0700

    rename FirebaseToken -> FirebaseDeviceToken

---
+++ db/migrate/20191021020557_create_firebase_device_token.rb
@@ -0,0 +1,7 @@
+class CreateFirebaseDeviceToken < ActiveRecord::Migration[5.2]
+  def change
+    create_table :firebase_device_tokens do |t|
+      t.string :token
+    end
+  end
+end
--- db/migrate/20191021020557_create_firebase_token.rb
+++
@@ -1,7 +0,0 @@
-class CreateFirebaseToken < ActiveRecord::Migration[5.2]
-  def change
-    create_table :firebase_tokens do |t|
-      t.string :token
-    end
-  end
-end
