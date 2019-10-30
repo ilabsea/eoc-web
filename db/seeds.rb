7d6a15f2df9de1e41b7f880a6cf0bde3e73aa910
radinreth <radin.reth@gmail.com>
2019-10-30 09:34:27 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Sop.destroy_all
 Category.destroy_all
 
 def create_sop sop
-  _sop = Sop.new name: sop[:name]
+  _sop = Sop.new name: sop[:name], tags: sop[:tags], description: sop[:description]
   file_path = Rails.root.join('public', 'seed', 'sops')
   _sop.with_attachment(file_path, sop[:file]) do |f|
     _sop.file = f
@@ -29,6 +29,8 @@ categories = [
         sops: [
           {
             name: 'cat 1 as sop',
+            tags: '#tag1 #tag2 #tag3',
+            description: 'some description cat 1 as sop',
             file: 'cat-1-as-sop.zip'
           },
           {
@@ -62,6 +64,8 @@ categories = [
         sops: [
           {
             name: 'sop 1.3.11',
+            tags: '#tag1 #tag2 #tag3',
+            description: 'some description cat 1 as sop',
             file: 'sop-1.3.11.rar'
           }
         ]
@@ -70,14 +74,20 @@ categories = [
     sops: [
       {
         name: 'sop 1.12',
+        tags: '#tag1 #tag2 #tag3',
+        description: 'some description cat 1 as sop',
         file: 'sop-1.1.pdf'
       },
       {
         name: 'sop 1.22',
+        tags: '#tag1 #tag2 #tag3',
+        description: 'some description cat 1 as sop',
         file: 'sop-1.22.rar'
       },
       {
         name: 'sop 1.3',
+        tags: '#tag1 #tag2 #tag3',
+        description: 'some description cat 1 as sop',
         file: nil
       }
     ]
@@ -99,6 +109,8 @@ categories = [
         sops: [
           {
             name: 'sop 2.1',
+            tags: '#tag1 #tag2 #tag3',
+            description: 'some description cat 1 as sop',
             file: 'sop-2.1.docx'
           }
         ]
@@ -113,6 +125,8 @@ categories = [
         sops: [
           {
             name: 'sop 2.2',
+            tags: '#tag1 #tag2 #tag3',
+            description: 'some description cat 1 as sop',
             file: 'sop-2.2.xlsx'
           }
         ]
@@ -133,6 +147,8 @@ categories = [
         sops: [
           {
             name: 'sop 2.3',
+            tags: '#tag1 #tag2 #tag3',
+            description: 'some description cat 1 as sop',
             file: nil
           }
         ]
@@ -143,6 +159,8 @@ categories = [
         sops: [
           {
             name: 'sop 2.4',
+            tags: '#tag1 #tag2 #tag3',
+            description: 'some description cat 1 as sop',
             file: 'sop-2.4.pdf'
           }
         ]
@@ -151,6 +169,8 @@ categories = [
     sops: [
       {
         name: 'sop 2',
+        tags: '#tag1 #tag2 #tag3',
+        description: 'some description cat 1 as sop',
         file: 'sop-2.docx'
       }
     ]
