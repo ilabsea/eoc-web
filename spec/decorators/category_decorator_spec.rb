8c8382efb4a8cd93786c0f9eb39a7666efdf64ba
radinreth <radin.reth@gmail.com>
2019-11-05 15:34:50 +0700

    update spec category decorator

---
+++ spec/decorators/category_decorator_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe CategoryDecorator do
+  let(:parent) { create(:category, name: 'viruses') }
+  
+  it { have_const_define(:WHITELIST_COLUMNS) }
+
+  it '#save' do
+    obj = described_class.new(name: 'test', parent: parent.name)
+    saved = obj.save 
+
+    expect(saved).to be_a(Category)
+    expect(saved.parent.name).to eq(parent.name)
+  end
+end
