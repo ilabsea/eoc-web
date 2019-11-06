52c026a0e4dfff66b7e188961d4ffa30ce4c50a3
radinreth <radin.reth@gmail.com>
2019-11-06 17:42:41 +0700

    clean importer

--- app/services/importer.rb
+++ app/services/importer.rb
@@ -6,20 +6,18 @@ class Importer
   def load
     xlsx = Roo::Spreadsheet.open(@file)
 
-    sheets = xlsx.sheets
-    sheets.each do |sheet_name|
+    xlsx.sheets.each do |sheet_name|
       sheet = xlsx.sheet(sheet_name)
       klazz = sheet_name.classify
 
       begin
         columns = "#{klazz}Decorator::WHITELIST_COLUMNS".constantize
-        sheet.each(columns) do |hash|
-          decorator = "#{klazz}Decorator".constantize.new(hash)
+        sheet.each(columns) do |row|
+          decorator = "#{klazz}Decorator".constantize.new(row)
           decorator.save
         end
       rescue Exception => e
-        msg = t('unprocessible', msg: e.message)
-        Rails.logger.warn(msg)
+        Rails.logger.warn( t('unprocessible', msg: e.message) )
       end
     end
   end
