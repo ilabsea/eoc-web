4ded4766fc783e15df7e55694729632db8ae249e
Borey Lim <lim.borey@gmail.com>
2019-11-19 11:02:24 +0700

    update database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,15 @@
 
 return unless Rails.env === "development"
 
+user = User.create(email: "admin@instedd.org", password: "password")
+user.confirm
+
 Category.__elasticsearch__.create_index! force: true
 Sop.__elasticsearch__.create_index! force: true
 Category.destroy_all
 Sop.destroy_all
 
-excel_path = Rails.root.join("public", "samples", "eoc.xlsx").to_s
+excel_path = Rails.root.join("sample", "eoc.xlsx").to_s
 raise "File #{File.basename(excel_path)} not exist" unless File.exist?(excel_path)
 
 importer = Importer.new(excel_path)
