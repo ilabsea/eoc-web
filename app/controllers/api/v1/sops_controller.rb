2eab2a1928182bad93b6e3e8bc995eaa9a5c5426
radinreth <radin.reth@gmail.com>
2019-11-21 15:53:32 +0700

    check mobile token

--- app/controllers/api/v1/sops_controller.rb
+++ app/controllers/api/v1/sops_controller.rb
@@ -4,14 +4,22 @@ module Api::V1
   class SopsController < ApiController
     def index
       @search = SearchService.text_search(sop_params)
-      respond_to do |format|
-        format.json { render json: @search }
+      if token == ENV["SERVER_SECRET_KEY_BASE"]
+        render json: @search
+      else
+        render json: { 
+          error: "Unauthorize request",
+          status: :bad_request }
       end
     end
 
     private
       def sop_params
         params.permit(:q, :page)
+      end
+
+      def token
+        @token ||= request.headers["Authorization"].split.last
       end
   end
 end
