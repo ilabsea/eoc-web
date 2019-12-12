241b3c265057ba110164bf005e51ad8ac9fa0f38
radinreth <radin.reth@gmail.com>
2019-12-12 14:50:13 +0700

    sops controller before filter check_secret_key

--- app/controllers/api/v1/sops_controller.rb
+++ app/controllers/api/v1/sops_controller.rb
@@ -2,6 +2,8 @@
 
 module Api::V1
   class SopsController < ApiController
+    before_action :check_secret_key
+
     def index
       @search = SearchService.text_search(sop_params)
       render json: @search.as_json(methods: [:model_name])
@@ -18,7 +20,14 @@ module Api::V1
       end
 
       def token
-        @token ||= request.headers["Authorization"].split.last
+        auth = request.headers["Authorization"]
+        @token ||= auth.split.last if auth
+      end
+
+      def check_secret_key
+        if token != ENV["SERVER_SECRET_KEY_BASE"]
+          render json: { error: "Unauthorize request", status: :bad_request }
+        end
       end
   end
 end
